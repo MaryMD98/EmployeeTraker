ad2f550c69b2845a9b19a2aa3ae28295d8542186
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 12:36:45 -0600

    added comments to index.js to describe functionality of the code

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 // acrodign to instructoronly need the index,js ad db folder
 // dist, helpers, lib, middleware, public, routes, src and server.js are extra
 
-//view all departments, view all roles, view all employees, add a department, 
-//add a role, add an employee, and update an employee role
-//
-
-
-//
-//
+///**************************************************** */
 /// main prompt to ask user what they would like to do.
-// choices: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+/// CHOICES: view all departments, view all roles, view all employees,  
+/// add a department, add a role, add an employee, and update an employee role
+/// *** will be called at the end of every funtion
+///**************************************************** */
+function mainP(){}
 
-
- 
- 
-
-
-
-// * update an employee role, will prompt questions
-// Queston: select an employee to update
-// and their new role and  updated it in the database
-//
+///**************************************************** */
+/// CHOICES from main prompt
+///**************************************************** */
 // * view all departments, will display departemnts table
 function view_DEPA (){}
 // * view all roles,will display roles table 
@@ -38,3 +29,7 @@ function add_ROLE (){}
 // Questions: the employee’s first name, last name,
 //  role, and manager,  add to the database 
 function add_EMPL (){}
+// * update an employee role, will prompt questions
+// Queston: select an employee to update
+// and their new role and  updated it in the database
+function update_EMPL(){}
