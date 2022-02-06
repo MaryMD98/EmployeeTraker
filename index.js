6fcf233eb4094c422d900359a8855e285d3c519e
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 12:29:53 -0600

    added functions to the choices the user will be prompt

--- index.js
+++ index.js
@@ -10,21 +10,31 @@
 //
 /// main prompt to ask user what they would like to do.
 // choices: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// * view all departments, will display departemnts table
-// * view all roles,will display roles table 
-// * view all employees, will display employee table 
-// * add a department, will prompt questions
-// question: the name of the department and add to the database  
-// * add a role, will prompt questions 
-// Question: the name, salary,
-//  and department for the role and add to the database
-// * add an employee, will prompt questions
-// Questions: the employee’s first name, last name,
-//  role, and manager,  add to the database 
+
+
+ 
+ 
+
+
+
 // * update an employee role, will prompt questions
 // Queston: select an employee to update
 // and their new role and  updated it in the database
 //
-//
-//
-//
+// * view all departments, will display departemnts table
+function view_DEPA (){}
+// * view all roles,will display roles table 
+function view_ROLE (){}
+// * view all employees, will display employee table
+function view_EMPL (){}
+// * add a department, will prompt questions
+// question: the name of the department and add to the database 
+function add_DEPA (){}
+// * add a role, will prompt questions 
+// Question: the name, salary,
+//  and department for the role and add to the database
+function add_ROLE (){}
+// * add an employee, will prompt questions
+// Questions: the employee’s first name, last name,
+//  role, and manager,  add to the database 
+function add_EMPL (){}
