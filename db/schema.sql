aace7c0709987cd7dc849d7fd4a4ad720a56af6f
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 00:46:34 -0600

    updated the schema file to create tables for department, employee, and role

--- db/schema.sql
+++ db/schema.sql
@@ -15,28 +15,21 @@ CREATE TABLE role(
     dep_id INT,
     role_title VARCHAR(30) NOT NULL,
     role_salary DECIMAL(8,2),
+    FOREIGN KEY (dep_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
+
 --   employee (role_id, first_name, last_name, manager_id)
-
-
--- role
-
--- id: INT PRIMARY KEY
-
--- title: VARCHAR(30) to hold role title
-
--- salary: DECIMAL to hold role salary
-
--- department_id: INT to hold reference to department role belongs to
-
--- employee
-
--- id: INT PRIMARY KEY
-
--- first_name: VARCHAR(30) to hold employee first name
-
--- last_name: VARCHAR(30) to hold employee last name
-
--- role_id: INT to hold reference to employee role
-
--- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
