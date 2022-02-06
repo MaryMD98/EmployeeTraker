5752d08c5556311fb7abf03a02f8af630be5a0f6
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 12:04:14 -0600

    updated the query to view all employees

--- db/query.sql
+++ db/query.sql
@@ -10,56 +10,40 @@ FROM department
 ORDER BY department.dep_name;
 
 -- view all roles function will display all job titles
-SELECT  role.role_title AS job_Title, 
-        role.id AS role_ID, 
-        department.dep_name AS department, 
-        role.role_salary AS salary
+SELECT  role.role_title AS Job_Title, 
+        role.id AS Role_ID, 
+        department.dep_name AS Department, 
+        role.role_salary AS Salary
 FROM role
 JOIN department 
 ON role.dep_id = department.id
 ORDER BY role.role_title;
 
 -- view all employees function will display all employee data
-
-
--- selft Join of employee data
-SELECT  associate.id, 
-        associate.first_name,
-        associate.last_name,
-        manager.first_name AS manager_first, manager.last_name AS manager_Last
+SELECT  associate.id AS ID,
+        associate.first_name AS First_Name,
+        associate.last_name AS Last_Name,
+        role.role_title AS Job_Title,
+        department.dep_name AS Department,
+        role.role_salary AS Salary,
+        CONCAT(manager.first_name, " ", manager.last_name) AS Manager
 FROM employee associate
-JOIN employee manager
-ON associate.manager_id = manager.id;
+LEFT JOIN employee manager ON associate.manager_id = manager.id
+LEFT JOIN role ON associate.role_id = role.id
+LEFT JOIN department ON role.dep_id = department.id
+ORDER BY associate.id;
 
 -----------------------------------------------------
 -- FOR REFERECE -- ** Testing different ways to display tables **
 -----------------------------------------------------
-
--- testing the all employees function
-
-SELECT  employee.id AS ID, 
-        employee.first_name AS First_Name, 
-        employee.last_name AS Last_Name, 
-        role.role_title AS job_Title, 
-        role.dep_id AS department, 
-        role.role_salary AS salary, 
-        employee.manager_id AS manager 
-FROM employee
-INNER JOIN role ON employee.role_id = role.id
-INNER JOIN employee ON employee.manager_id = employee.id
-INNER JOIN department ON role.dep_id = department.id
-JOIN role
-ON employee.role_id = role.id
-ON employee.manager_id = employee.id
-ORDER BY employee.id;
-
-
-SELECT *
-FROM department
-INNER JOIN role ON role.dep_id = department.id 
-INNER JOIN employee ON employee.role_id = role.id
--- INNER JOIN employee ON employee.manager_id = employee.id
-ORDER BY employee.id;
+-- selft Join of employee data
+SELECT  associate.id, 
+        associate.first_name,
+        associate.last_name,
+        CONCAT(manager.first_name, " ", manager.last_name) AS manager
+FROM employee associate
+LEFT JOIN employee manager
+ON associate.manager_id = manager.id;
 
 -- tests to see two comulns of data in different ways
 SELECT department.dep_name AS department, role.role_title
