737ebf314cecaf1a3ac3d849c67602138da774f0
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 00:54:09 -0600

    updated the schema file to display both foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES role(id),
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
