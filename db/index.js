c17adf5ea9e1aa715e7868a9f2d594f6f8cf001c
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 18:26:37 -0600

    updated the conncetion.js and added function to view all departments

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const db = require('./connection.js');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// app.post();
+
+// app.listen(PORT, () => {
+//     console.log(`server running on oprt ${PORT}`);
+// });
+
+// Display information of the departments
+function view_depa (data){
+
+}
