696076ff3f614bdba87de41b44aca5014004ea33
Maribel Montes <maribel.doris@gmail.com>
2022-02-06 19:43:26 -0600

    fixed issue in connection.js

--- db/connection.js
+++ db/connection.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: process.user.DB_USER,
-        pasword: process.env.DB_PASSWORD,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     },
     console.log(`Connected to the company_db`)
